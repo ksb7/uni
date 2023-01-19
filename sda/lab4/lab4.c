a00962cd55ade08b9d109a223fb40671106ebbd2
kiu <kiu@pop-os.localdomain>
2023-01-20 01:41:27 +0200

    update linked list

--- sda/lab4/lab4.c
+++ sda/lab4/lab4.c
@@ -1,37 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/*  Fie dată o listă de numere reale. Pentru fiecare element al listei
+să se tipărească numărul elementelor negative, ce-l succed. */
+
 struct node
 {
 	float val;
 	struct node *head;
 };
-void node_insert(struct node **initial, int valoare)
+
+void node_insert(struct node *initial, int valoare)
 {
-	struct node *new = (struct node*) malloc(sizeof(struct node));
-	if(new == NULL)
+	struct node *temp = NULL;
+	struct node *curent = (struct node*) malloc(sizeof(struct node));
+	if(curent== NULL)
 	{
 		printf("Alocare esuata\n");
 		exit(1);
-	} else
-	{
-		new->val = valoare;
-		new->head = (*initial);
-		(*initial) = new;
 	}
+	
+	curent->val = valoare;
+	curent->head = NULL;
+	
+	if (initial == NULL) 
+	{
+	    initial = curent; //pentru primul nod
+	}
+	
+	while(temp->head != NULL)
+	{
+	    temp = temp->head; //trece prin toate nodurile pana ajunge la ultimul
+	}
+	temp->head = curent; //adauga noul nod la capat
 }
+/*void find(struct node *original, int n)
+{
+    struct node *temp = original;
+    for(int i = 0; i < n; i++)
+    {
+        while (node->head != NULL)
+        {
+            if(node)
+        }
+    }
+}
+*/
 int main()
 {
 	printf("Introduceti numarul de noduri\n");
 	int n;
 	scanf("%d", &n);
-	struct node *N = (struct node*)malloc(sizeof(struct node));
+	struct node *N = NULL;
 	float element;
+	printf("introduceti elementele: \n");
 	for(int i = 0; i < n; i++)
 	{
-		printf("introduceti elementul: \n");
 		scanf("%f", &element);
-		node_insert(&N, element);
+		node_insert(N, element);
 	}
-
+	while(N != NULL)
+    {
+        printf("Data = %f\n", N->val); // Print data of current node
+        N = N->head;                 // Move to next node
+    }
 	return 0;
 }
+
+
+
+
+
