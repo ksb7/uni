1b2a868a4ab88b22c8d73859543e231553c88c9c
ksb7 <70291457+ksb7@users.noreply.github.com>
2023-03-06 17:07:53 -0500

    added buggy ford alg

--- md/lab2/lab2.c
+++ md/lab2/lab2.c
@@ -1,70 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
 
 int **allocMatrix();
-void createMatrix(int **matrice);
+void createMatrix(int **matrice, int varf);
 void input(int **matrice, int varf);
 void Ford(int **matrice, int varf);
+int cmpArr(int *A, int *B);
 int main()
 {
     int **MA; //matricea ponderata de adiacenta
-    createMatrix(MA);
     int choice;
+    printf("Numarul de varfuri\n");
+    int varf;
+    scanf("%d", &varf);
+    createMatrix(MA, varf);
     printf("Meniu:\n1. Algoritmul Ford\n2.Algoritmul Bellman-Kalaba\nAlegerea: ");
     scanf("%d", &choice);
     switch(choice)
     {
         case 1: 
         {
-            printf("1. Drumul minim\n2.Drumul maxim\nAlegerea: ");
-            int choice;
-            scanf("%d", &choice);
-            switch(choice)
-            {
-                case 1: 
-                {
-                    break;
-                }
-                case 2:
-                {
-                    break;
-                }
-            }
+            Ford(MA, varf);
             break;
         }
         case 2:
         {
-            printf("1. Drumul minim\n2.Drumul maxim\nAlegerea: ");
+            /*printf("1. Drumul minim\n2.Drumul maxim\nAlegerea: ");
             int choice;
             scanf("%d", &choice);
             switch(choice)
             {
                 case 1: 
                 {
+                    
                     break;
                 }
                 case 2:
                 {
                     break;
                 }
-            }
+            }*/
             break;
         }
     }
     return 0;
 }
-void createMatrix(int **matrice)
+void createMatrix(int **matrice, int varf)
 {
-    printf("Numarul de varfuri\n");
-    int varf;
-    scanf("%d", &varf);
     matrice = allocMatrix(varf);
     input(matrice, varf);
 }
 int **allocMatrix(int varf)
 {
-    int **matrice = malloc(varf*sizeof(int *));
+    int **matrice = malloc(varf*sizeof(int*));
     if(!matrice)
     {
         printf("Alocare esuata");
@@ -86,7 +75,7 @@ void input(int **matrice, int varf)
     printf("Dati matricea ponderata de adiacenta (infinit == 100)\n");
     for(int i = 0; i < varf; i++)
     {
-        for(int j = 0, k = 0; j < varf, k < i-1; j++, k++)
+        for(int j = 0; j < varf; j++)
         {
             scanf("%d", &matrice[i][j]);
             /*TODO:
@@ -101,7 +90,57 @@ void input(int **matrice, int varf)
         }
     }
 } 
+int cmpArr(int *A, int *B)
+{
+    int Asize = sizeof(A) / sizeof(A[0]);
+    int Bsize = sizeof(B) / sizeof(B[0]);
+    if(Asize == Bsize)
+    {
+        for(int i = 0; i < Asize; i++)
+        {
+            for(int j = 0; j < Bsize; j++)
+            {
+                if(A[i] != B[j]) return 1;
+            }
+        }
+    } else return 1;
+    return 0;
+}
 void Ford(int **matrice, int varf)
 {
-    
+    printf("1. Drumul minim\n2.Drumul maxim\nAlegerea: ");
+        int choice;
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1: 
+            {
+                int *H = malloc(varf * sizeof(int));
+                int *H2 = malloc(varf * sizeof(int));
+                for(int i = 0; i < varf; i++) H2[i] = 100;
+                H2[0] = 0;
+                do{
+                for(int i = 0; i < varf; i++)
+                {
+                    H[i] = H2[i];
+                    for(int j = 0; j < varf; j++)
+                    {
+                        if(matrice[i][j] != 0 && matrice[i][j] != 100)
+                        {
+                            if(H2[j] - H[i] > matrice[i][j])
+                            {
+                                H2[j] = H[i] + matrice[i][j];
+                                printf("H %d, H2 %d\n", H[i], H2[i]);
+                            }
+                        }
+                    } 
+                }
+                }while(cmpArr(H, H2)!= 0);
+                break;
+            }
+            case 2:
+            {
+                break;
+            }
+        }
 }
