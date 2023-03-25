d0bc6ce0960ef7a8f70a981c741ff1e9593c96a4
ksb7 <70291457+ksb7@users.noreply.github.com>
2023-03-25 18:14:50 +0200

    added stuff

--- md/lab2/lab2.c
+++ md/lab2/lab2.c
@@ -115,79 +115,169 @@ int cmpArr(int *A, int *B, int varf)
 }
 void cpyArr(int *A, int *B, int varf)
 {
-    /*int Asize = sizeof(A) / sizeof(A[0]);
-    int Bsize = sizeof(B) / sizeof(B[0]);
-    if(Asize == Bsize)
-    {*/
-        for(int i = 0; i < varf; i++)
-        {
-            A[i] = B[i];
-        }
-    /*} else 
+    for(int i = 0; i < varf; i++)
     {
-        printf("Eroare la matricea de varfuri\n");
-        exit(1);
-    }*/
+        A[i] = B[i];
+    }
 }
+/*void calculFord(int choice)
+{
+    int *H = calloc(varf, sizeof(int));
+    int *H2 = calloc(varf, sizeof(int));
+    if(choice == 1) 
+    {
+        for(int i = 0; i < varf; i++) H2[i] = 100;
+    } else if(choice == 2)
+    {
+        for(int i = 0; i < varf; i++) H2[i] = -100;
+    }
+    H2[0] = 0;
+    do{
+    for(int i = 0; i < varf; i++)
+    {  
+        cpyArr(H, H2, varf);
+        cpyArr(H2, H, varf);
+        for(int j = 0; j < varf; j++)
+        {
+           if(i != j && (matrice[i][j] != 100 || matrice[i][j] != -100))
+           {
+                switch(choice)
+                {
+                    case 1:
+                    { 
+                        if(H2[j] - H[i] > matrice[i][j])
+                        { 
+                            H2[j] = H[i] + matrice[i][j];
+                        }
+                    }
+                    case 2:
+                    {
+                        if(H2[j] - H[i] < matrice[i][j])
+                        {
+                            H2[j] = H[i] + matrice[i][j];
+                        }
+                    }
+                    
+            }
+        }
+    }
+    }while(cmpArr(H, H2, varf)!= 0);
+        int *pozitii = calloc(1, sizeof(int));
+        int c = 0;
+        pozitii[c] = varf; //stabilim varful din top
+        for(int i = varf-1; i >= 0; i--)
+        {
+            for(int j = varf-1; j > 0; j--)
+		    {	
+	            if(j<i)
+		       {
+                    if(H2[i] - H[j] == matrice[j][i]) 
+                    { 
+                        c++;
+                        pozitii = realloc(pozitii, (c+1)*sizeof(int));
+                        pozitii[c] = i-1; //determinam drumul dintre punctul initial si final
+                    }
+                }
+            }
+		}
+    if(H2[pozitii[c++]] - H[0]) pozitii[c++] = 1; //determinam daca este drum de la punctul initial
+	printf("Drumul este"); 
+	for(int i = c-1; i >= 0; i--)
+	{
+	    printf(" %d ", pozitii[i]); 
+    }
+}*/
 void Ford(int matrice[7][7], int varf)
 {
     printf("1. Drumul minim\n2.Drumul maxim\nAlegerea: ");
         int choice;
         scanf("%d", &choice);
-        switch(choice)
+        int *H = calloc(varf, sizeof(int));
+    int *H2 = calloc(varf, sizeof(int));
+    switch(choice)
+    {
+        case 1:
         {
-            case 1: 
+            for(int i = 0; i < varf; i++) 
             {
-                int *H = calloc(varf, sizeof(int));
-                int *H2 = calloc(varf, sizeof(int));
-                for(int i = 0; i < varf; i++) H2[i] = 100;
-                H2[0] = 0;
-                int count =0;
-                do{
-                    
-                for(int i = 0; i < varf; i++)
-                {  
-                    cpyArr(H, H2, varf);
-                    cpyArr(H2, H, varf);
-                    //printf("count %d", count);
-                    for(int j = 0; j < varf; j++)
-                    {
-                        printf("-------------------H[%d] %d, H2[%d] %d ---\n", i, H[i], j, H2[j]);
-                       if(i != j && matrice[i][j] != 100)
-                       {
-                            if(H2[j] - H[i] > matrice[i][j])
-                            { 
-                                printf("1. H[%d] %d, H2[%d] %d matrice %d\n", i, H[i], j, H2[j], matrice[i][j]);
-                                H2[j] = H[i] + matrice[i][j];
-                                printf("2. H[%d] %d, H2[%d] %d matrice %d\n \n", i, H[i],  j, H2[j], matrice[i][j]);
-                            }
-                            printf("H[%d] %d, H2[%d] %d ---\n", i, H[i], j, H2[j]);
-                       }
-                    } 
-                    
-                }count++;
-                }while(cmpArr(H, H2, varf)!= 0);
-
-                for(int i = varf-1; i >= 0; i--)
-                    {
-                    for(int j = varf-1; j >= 0; j--)
-		    {	//printf("%d ", matrice[varf-i-1][varf-j-1]); 
-				  // printf(" H2 %d H %d", H2[i], H[j]);
-	                 if(j<i)
-		            {
-                        if(H2[i] - H[j] == matrice[varf-i-1][varf-i-1]) 
-                        {
-                           printf("drumul minim %d diferenta %d-%d = %d\n", H[i], H2[j], H[i], matrice[j][i]);
-			    printf("h2 %d h1 %d\n", H2[j], H[i]);
-           }
-                    }
-                }printf("\n");
-		    }
-                break;
-	} 
-            case 2:
-            {
-                break;
+                H2[i] = 100;
             }
+            break;
         }
+        case 2:
+        {
+            for(int i = 0; i < varf; i++) 
+            {
+                H2[i] = -100;
+            }
+            break;
+        }
+    }
+    H2[0] = 0;
+    do{
+    for(int i = 0; i < varf; i++)
+    {  
+        cpyArr(H, H2, varf);
+        cpyArr(H2, H, varf);
+        for(int j = 0; j < varf; j++)
+        {
+           if(i != j && matrice[i][j] != 100)
+           {
+                switch(choice)
+                {
+                    case 1:
+                    { 
+                        if(H2[j] - H[i] > matrice[i][j])
+                        { 
+                            H2[j] = H[i] + matrice[i][j];
+                        }
+                        break;
+                    }
+                    case 2:
+                    {
+                        if(H2[j] - H[i] < matrice[i][j])
+                        {
+                            printf("1. H[%d] %d, H2[%d] %d matrice %d\n", i, H[i], j, H2[j], matrice[i][j]);
+                            H2[j] = H[i] + matrice[i][j];
+                            printf("2. H[%d] %d, H2[%d] %d matrice %d\n \n", i, H[i],  j, H2[j], matrice[i][j]);
+                        }
+                         
+                        break;
+                    }
+                }
+                printf("H[%d] %d, H2[%d] %d ---\n", i, H[i], j, H2[j]);
+        }
+    }
+    }
+    }while(cmpArr(H, H2, varf)!= 0);
+    for(int i = 0; i < varf; i++)
+    {
+        printf("H %d \n", H[i]);
+    }
+        int *pozitii = calloc(1, sizeof(int));
+        int c = 0;
+        pozitii[c] = varf; //stabilim varful din top
+        for(int i = varf-1; i >= 0; i--)
+        {
+            for(int j = varf-1; j > 0; j--)
+		    {	
+	            if(j<i)
+		       {
+                    if(H2[i] - H[j] == matrice[j][i]) 
+                    { 
+                        c++;
+                        pozitii = realloc(pozitii, (c+1)*sizeof(int));
+                        pozitii[c] = i-1; //determinam drumul dintre punctul initial si final
+                        printf("drumul minim %d diferenta %d-%d = %d\n", H[i], H2[i], H[j], matrice[j][i]);
+			    printf("h2 %d h1 %d\n", H2[j], H[i]);
+                    }
+                }
+            }
+		}
+    if(H2[pozitii[c++]] - H[0]) pozitii[c++] = 1; //determinam daca este drum de la punctul initial
+	printf("Drumul este"); 
+	for(int i = c-1; i >= 0; i--)
+	{
+	    printf(" %d ", pozitii[i]); 
+    }
 }
